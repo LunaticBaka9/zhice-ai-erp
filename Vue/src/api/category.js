9d2b6fc6fc44b4145b7162ea65e15ed9e949fda7
LunaticBaka9 <347254238@qq.com>
2026-08-26 00:25:41 +0800

    style: Vue文件夹修改成vue

--- Vue/src/api/category.js
+++
@@ -1,5 +0,0 @@
-import request from "@/utils/request";
-
-export function getCategoryTree() {
-    return request.get("/category/tree");
-}
--- Vue/src/api/operationLog.js
+++
@@ -1,9 +0,0 @@
-import request from "@/utils/request";
-
-export function getLogList(params) {
-    return request.get("/operationLog/list", { params });
-}
-
-export function deleteAllLogs() {
-    return request.post("/operationLog/deleteAll");
-}
